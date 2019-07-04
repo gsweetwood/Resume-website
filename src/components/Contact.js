c874abf5739f99606581a1c60e5bc5deaf7f41bd
gsweetwood <garretsweetwood6@gmail.com>
2019-07-04 14:53:46 -0700

    Added resume and smooth scrolling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,7 @@ import SectionHeader from "./sub-components/SectionHeader";
 import emailIcon from "../images/icons/email-icon.png";
 import linkedinIcon from "../images/icons/linkedin-icon.png";
 import gitIcon from "../images/icons/git-icon.png";
+import docIcon from "../images/icons/download-resume-icon.png";
 
 const Contact = () => (
   <section id="contact">
@@ -24,6 +25,14 @@ const Contact = () => (
       <a href="https://github.com/gsweetwood" className="contact-item">
         <img src={gitIcon} className="contact-icon" id="git-icon" />
         <div>github.com/garretsweetwood</div>
+      </a>
+
+      <a
+        href="https://drive.google.com/file/d/1sG24YdX2TBJ_7-NYezE6oZMMfiHYITc4/view?usp=sharing"
+        className="contact-item"
+      >
+        <img src={docIcon} className="contact-icon" id="resume-icon" />
+        <div>Downloadable Resume</div>
       </a>
     </div>
     <div className="created-by">Created by Garret Sweetwood</div>
