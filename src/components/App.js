ef1f13d1f166bcb7cc1cfdaf1566d42bd4a7c410
gsweetwood <garretsweetwood6@gmail.com>
2019-09-01 15:32:54 -0700

    added animation and styling changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import * as Scroll from "react-scroll";
+import { scroller } from "react-scroll";
 import "../styles/styles.scss";
 import About from "./About";
 import Experience from "./Experience";
@@ -10,6 +12,11 @@ import Contact from "./Contact";
 import Welcome from "./Welcome";
 
 class App extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      scroller.scrollTo("about-section", { duration: 1000, offset: -50 });
+    }, 3500);
+  }
   render() {
     return (
       <div className="App">
